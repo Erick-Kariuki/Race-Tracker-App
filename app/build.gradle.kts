9baeacef9750e45225a34076cce0fece227da275
Erick-Kariuki <ithandierick4@gmail.com>
2026-01-30 09:27:50 +0300

    Add test depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation(kotlin("test"))
 }
